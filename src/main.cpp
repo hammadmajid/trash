2c5cbce8e8813aeafcb15ed643e70f0964706d6b
Hammad Majid <hammadmajid@proton.me>
2025-07-27 23:20:16 +0500

    Draw menu title

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,36 @@
+#include <string>
+
 #include "raylib.h"
 
-int main(void)
-{
-    InitWindow(800, 450, "raylib [core] example - basic window");
+void drawMenu() {
+    const int screenWidth = GetScreenWidth();
+    const int screenHeight = GetScreenHeight();
 
-    while (!WindowShouldClose())
-    {
+    const int startYPos = screenHeight / 6;
+    constexpr int fontSize = 32;
+
+    const auto menuTitle = "Trash";
+
+    const int menuTitleWidth = MeasureText(menuTitle, fontSize);
+    const int menuTitlePosX = (screenWidth / 2) - (menuTitleWidth / 2);
+    DrawText(menuTitle, menuTitlePosX, startYPos, fontSize, LIGHTGRAY);
+
+    const int lineWidth = screenWidth / 4;
+    const int lineStartX = (screenWidth - lineWidth) / 2;
+    DrawLine(lineStartX, startYPos + 36, lineStartX + lineWidth, startYPos + 36, LIGHTGRAY);
+}
+
+
+int main() {
+    SetConfigFlags(FLAG_FULLSCREEN_MODE);
+    InitWindow(GetScreenWidth(), GetScreenHeight(), "Trash");
+
+    while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        ClearBackground(DARKGRAY);
+
+        drawMenu();
+
         EndDrawing();
     }
 
