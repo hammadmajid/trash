57a4b15b948b968cb7b6e827361482088babebf3
Hammad Majid <hammadmajid@proton.me>
2025-07-28 00:45:35 +0500

    Refactor menu logic into multiple functions and use vector for menu list

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,65 @@
+#include <vector>
 #include <string>
 
 #include "raylib.h"
 
-void drawMenu(int selectedIndex) {
-    const int screenWidth = GetScreenWidth();
-    const int screenHeight = GetScreenHeight();
+constexpr int FONT_SIZE = 32;
+constexpr int MENU_SPACING = 36;
 
-    const int startYPos = screenHeight / 6;
-    constexpr int fontSize = 32;
-
-    const auto menuTitle = "Trash";
-    const int menuTitleWidth = MeasureText(menuTitle, fontSize);
-    const int menuTitlePosX = (screenWidth / 2) - (menuTitleWidth / 2);
-    DrawText(menuTitle, menuTitlePosX, startYPos, fontSize, LIGHTGRAY);
+void drawMenuTitle(const std::string &title, const int screenWidth, const int startYPos) {
+    const int titleWidth = MeasureText(title.c_str(), FONT_SIZE);
+    const int titlePosX = (screenWidth - titleWidth) / 2;
+    DrawText(title.c_str(), titlePosX, startYPos, FONT_SIZE, LIGHTGRAY);
 
     const int lineWidth = screenWidth / 4;
     const int lineStartX = (screenWidth - lineWidth) / 2;
-    DrawLine(lineStartX, startYPos + 36, lineStartX + lineWidth, startYPos + 36, LIGHTGRAY);
+    DrawLine(lineStartX, startYPos + FONT_SIZE + 4, lineStartX + lineWidth, startYPos + FONT_SIZE + 4, LIGHTGRAY);
+}
 
-    const char *menuItems[] = {
-        "Play", "Scoreboard", "Exit"
-    };
-    constexpr int menuLength = std::size(menuItems);
+void drawMenuItems(const std::vector<std::string> &items, const int selectedIndex, const int screenWidth,
+                   const int startYPos) {
+    for (size_t i = 0; i < items.size(); ++i) {
+        const int textWidth = MeasureText(items[i].c_str(), FONT_SIZE);
+        const int textPosX = (screenWidth - textWidth) / 2;
+        const int textPosY = startYPos + MENU_SPACING * (static_cast<int>(i) + 1);
 
-    for (int i = 0; i < menuLength; i++) {
-        const int textWidth = MeasureText(menuItems[i], fontSize);
-        const int textPosX = (screenWidth / 2) - (textWidth / 2);
-
-        if (selectedIndex == i) {
-            DrawText(menuItems[i], textPosX, startYPos + (36 * (i + 1)), fontSize, DARKBLUE);
-        } else {
-            DrawText(menuItems[i], textPosX, startYPos + (36 * (i + 1)), fontSize, LIGHTGRAY);
-        }
+        const Color color = (static_cast<int>(i) == selectedIndex) ? DARKBLUE : LIGHTGRAY;
+        DrawText(items[i].c_str(), textPosX, textPosY, FONT_SIZE, color);
     }
 }
 
+void drawMenu(const std::vector<std::string> &menuItems, const int selectedIndex) {
+    const int screenWidth = GetScreenWidth();
+    const int screenHeight = GetScreenHeight();
+    const int startYPos = screenHeight / 6;
+    const std::string menuTitle = "Trash";
+
+    drawMenuTitle(menuTitle, screenWidth, startYPos);
+    drawMenuItems(menuItems, selectedIndex, screenWidth, startYPos);
+}
 
 int main() {
     SetConfigFlags(FLAG_FULLSCREEN_MODE);
     InitWindow(GetScreenWidth(), GetScreenHeight(), "Trash");
 
+    const std::vector<std::string> menuItems{"Play", "Scoreboard", "Exit"};
     int selectedIndex = 0;
 
     while (!WindowShouldClose()) {
         if (IsKeyPressed(KEY_DOWN)) {
-            selectedIndex = (selectedIndex + 1) % 3;
+            selectedIndex = (selectedIndex + 1) % static_cast<int>(menuItems.size());
         }
         if (IsKeyPressed(KEY_UP)) {
-            selectedIndex = (selectedIndex - 1 + 3) % 3;
+            selectedIndex = (selectedIndex - 1 + static_cast<int>(menuItems.size())) % static_cast<int>(menuItems.
+                                size());
         }
 
         BeginDrawing();
         ClearBackground(DARKGRAY);
-
-        drawMenu(selectedIndex);
-
+        drawMenu(menuItems, selectedIndex);
         EndDrawing();
     }
 
     CloseWindow();
-
     return 0;
 }
